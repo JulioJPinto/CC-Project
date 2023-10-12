24963ae93819698ee8e58299376b924a0137d6d7
Carlos-oficial <carlosribeiro2003oficial@gmail.com>
2023-10-12 12:29:43 +0100

    add: TCP client server boilerplate

---
+++ server/go.mod
@@ -0,0 +1,3 @@
+module example.com/cc_project/server
+
+go 1.21.3
---
+++ server/server.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8080")
+	if err != nil {
+		fmt.Println("Error listening:", err)
+		return
+	}
+	defer listener.Close()
+
+	fmt.Println("Server is listening on localhost:8080")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+			continue
+		}
+
+		go handleClient(conn)
+	}
+}
+
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+	fmt.Println("Accepted connection from", conn.RemoteAddr())
+
+	// Handle client requests here
+}
