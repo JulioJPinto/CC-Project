d01141ee0944f7f8a4db92547d5cd8b38c7102ee
Carlos-oficial <carlosribeiro2003oficial@gmail.com>
2023-11-06 15:15:39 +0000

    client state

--- src/client/lib/actions.go
+++ src/client/lib/actions.go
@@ -1,15 +1,26 @@
-package actions
+package lib
 
 import (
+	"cc_project/helpers"
 	"cc_project/protocol/fstp"
 	"fmt"
 	"os"
 	"path/filepath"
-	"strings"
 )
 
-func MakeDirectoryAvailable(client *fstp.FSTPclient, directory string) error {
+
+type Client struct {
+	state struct {
+		peers          helpers.Set[fstp.DeviceIdentifier]
+		known_files    map[fstp.FileHash]fstp.FileMetaData
+		known_segments map[fstp.DeviceIdentifier]fstp.FileSegment
+	}
+	FSTPclient *fstp.FSTPclient
+}
+
+func MakeDirectoryAvailable(client *Client, directory string) error {
 	_, err := os.Stat(directory)
+	fstp_client := client.FSTPclient
 
 	visitFile := func(fp string, fi os.DirEntry, err error) error {
 		if err != nil {
@@ -27,9 +38,9 @@ func MakeDirectoryAvailable(client *fstp.FSTPclient, directory string) error {
 			fmt.Printf("File hashed: %s\n", fp)
 			fmt.Printf("Hashed data: %+v\n", fdata)
 
-			fdata.OriginatorIP = client.Conn.LocalAddr().String()
+			fdata.OriginatorIP = fstp_client.Conn.LocalAddr().String()
 			fdata.Name = filepath.Base(fp)
-			client.Request(fstp.IHaveFileRequest(fstp.IHaveFileReqProps(*fdata)))
+			fstp_client.Request(fstp.IHaveFileRequest(fstp.IHaveFileReqProps(*fdata)))
 		}
 
 		return nil
@@ -41,76 +52,29 @@ func MakeDirectoryAvailable(client *fstp.FSTPclient, directory string) error {
 	return err
 }
 
-func MakeFileAvailable(client *fstp.FSTPclient, f_path string) error {
+func MakeFileAvailable(client *Client, f_path string) error {
 	fileInfo, err := os.Stat(f_path)
+	fstp_client := client.FSTPclient
 	if err != nil {
 		return err
 	}
 	if fileInfo.IsDir() {
 		return fmt.Errorf("%s is a directory", f_path)
 	} else {
-
+		fdata, _ := fstp.HashFile(f_path)
+		if err != nil {
+			return err
+		} else {
+			fdata.OriginatorIP = fstp_client.Conn.LocalAddr().String()
+			fdata.Name = filepath.Base(f_path)
+			fstp_client.Request(fstp.IHaveFileRequest(fstp.IHaveFileReqProps(*fdata)))
+		}
 	}
 	return nil
 }
 
-type StatusMessage struct {
-	Messages []string
-	Errors   []error
-}
-
-func (m *StatusMessage) ShowMessages() string {
-	var ret string
-	ret += "Messages:\n"
-	for _, msg := range m.Messages {
-		ret += "\t" + msg + "\n"
-	}
-	return ret
-}
-func (m *StatusMessage) ShowErrors() string {
-	var ret string
-	ret += "Errors:\n"
-	for _, err := range m.Errors {
-		ret += "\t" + err.Error() + "\n"
-	}
-	return ret
-}
-
-func (m *StatusMessage) Show() string {
-	var ret string
-	ret += "Messages:\n"
-	for _, msg := range m.Messages {
-		ret += "\t" + msg + "\n"
-	}
-	ret += "Errors:\n"
-	for _, err := range m.Errors {
-		ret += "\t" + err.Error() + "\n"
-	}
-	return ret
-}
-
-func (m *StatusMessage) Error() error {
-	err_strings := make([]string, len(m.Errors))
-	for i, e := range m.Errors {
-		err_strings[i] = e.Error()
-	}
-	return fmt.Errorf(strings.Join(err_strings, ";"))
-}
-func (m *StatusMessage) AddError(err error) {
-	m.Errors = append(m.Errors, err)
-}
-
-func (m *StatusMessage) AddMessage(err error, success_message string) {
-	if err == nil {
-		m.Messages = append(m.Messages, success_message)
-	} else {
-		m.AddError(err)
-	}
-}
-
-func UploadFile(client *fstp.FSTPclient, args []string) StatusMessage {
-	fmt.Printf("Uploading file: %s\n", args[0])
-	ret := StatusMessage{}
+func UploadFile(client *Client, args []string) helpers.StatusMessage {
+	ret := helpers.StatusMessage{}
 	for _, arg := range args {
 		ret.AddMessage(MakeFileAvailable(client, arg), fmt.Sprintf("File %s uploaded", arg))
 	}
--- src/client/main.go
+++ src/client/main.go
@@ -2,41 +2,41 @@ package main
 
 import (
 	"bufio"
-	actions "cc_project/client/lib"
+	"cc_project/client/lib"
+	"cc_project/helpers"
 	"cc_project/protocol/fstp"
 	"fmt"
+	"github.com/fatih/color"
 	"log"
 	"os"
 	"strings"
-
-	"github.com/fatih/color"
 )
 
-var commands = map[string]func(*fstp.FSTPclient, []string) actions.StatusMessage{
-	"upload": actions.UploadFile,
+var commands = map[string]func(*lib.Client, []string) helpers.StatusMessage{
+	"upload": lib.UploadFile,
 }
 
-func main() {
 
+func main() {
+	client := &lib.Client{}
 	config := fstp.FSTPConfig{Host: "localhost", Port: "8080"}
-	client, err := fstp.NewFSTPClient(config)
+	var err error
+
+	client.FSTPclient, err = fstp.NewFSTPClient(config)
+
 	if err != nil {
 		color.Red(err.Error())
 		return
 	}
 	if len(os.Args) > 1 {
-		actions.MakeDirectoryAvailable(client, os.Args[1])
+		lib.MakeDirectoryAvailable(client, os.Args[1])
 	}
 
-	// body := fstp.IHaveProps{Files: []fstp.FileInfo{{Id: 1}}}
-	// client.Request(fstp.IHaveRequest(body))
 	fdata, _ := fstp.HashFile("/home/cralos/Uni/3Ano/CC/CC-Project/client_files/test_file.txt")
-	fdata.OriginatorIP = client.Conn.LocalAddr().String()
+	fdata.OriginatorIP = client.FSTPclient.Conn.LocalAddr().String()
 	fdata.Name = "test_file.txt"
-	// client.Request(fstp.IHaveFileRequest(fstp.IHaveFileReqProps(*fdata)))
-	client.Request(fstp.AllFilesRequest())
-	// make_file_available("client_files/test_file.txt")
-	// Send and receive data with the server
+	client.FSTPclient.Request(fstp.AllFilesRequest())
+
 	reader := bufio.NewReader(os.Stdin)
 	for {
 		line, err := reader.ReadString('\n')
@@ -53,6 +53,8 @@ func main() {
 		}
 		status := f(client, split_line[1:])
 		color.Green(status.ShowMessages())
-		color.Red(status.ShowErrors())
+		if status.Error() != nil {
+			color.Red(status.ShowErrors())
+		}
 	}
 }
