cca79f48354181192a1be14b5dcd418b299b6dfd
Carlos-oficial <carlosribeiro2003oficial@gmail.com>
2023-11-30 13:13:54 +0000

    download.go

---
+++ node/lib/download.go
@@ -0,0 +1,86 @@
+package lib
+
+import (
+	"cc_project/helpers"
+	"cc_project/protocol"
+	"cc_project/protocol/fstp"
+	"cc_project/protocol/p2p"
+	"encoding/json"
+	"fmt"
+	"time"
+)
+
+func (node *Node) DownloadFile(file_hash protocol.FileHash) error {
+	if _, ok := node.Chanels.Get(file_hash); ok {
+		return fmt.Errorf("download already in progress")
+	}
+
+	file_meta_data, ok := node.KnownFiles[file_hash] // file_meta_data,ok := c.KnownFiles.get(file_hash)
+
+	if !ok {
+		node.FetchFiles(nil)
+		file_meta_data, ok = node.KnownFiles[file_hash]
+		if !ok {
+			return fmt.Errorf("files does not exist: %v", file_hash)
+		}
+	}
+
+	resp, err := node.FSTPclient.Request(fstp.NewWhoHasRequest(fstp.WhoHasReqProps{File: file_hash}))
+	if err != nil {
+		return err
+	}
+	pay, ok := resp.Payload.(*fstp.WhoHasRespProps)
+	if !ok {
+		return fmt.Errorf("invalid payload")
+	}
+	p := map[protocol.DeviceIdentifier][]protocol.FileSegment(*pay)
+	if resp.Header.Flags == fstp.ErrResp {
+		err_resp := resp.Payload.(*fstp.ErrorResponse)
+		return fmt.Errorf(err_resp.Err)
+	}
+	go node.send_segment_requests(p)
+	go node.await_segment_responses(file_meta_data)
+
+	return nil
+}
+
+func (node *Node) send_segment_requests(m map[protocol.DeviceIdentifier][]protocol.FileSegment) {
+	for id, segments := range m {
+		for _, segment := range segments {
+			node.RequestSegment(id, segment)
+		}
+	}
+}
+
+func (node *Node) await_segment_responses(file protocol.FileMetaData) {
+	ch, _ := node.Chanels.Get(file.Hash)
+	for msg := range ch {
+		println(json.Marshal(msg))
+	}
+
+}
+
+func (node *Node) Distribute(device_segments map[protocol.DeviceIdentifier][]protocol.FileSegment, metadata protocol.FileMetaData) map[int64]protocol.DeviceIdentifier {
+	ret := make(map[int64]protocol.DeviceIdentifier)
+	for n := int64(0); n < int64(metadata.Length/protocol.SegmentLength); n++ {
+		for device, segments := range device_segments {
+			for _, segment := range segments {
+				if segment.BlockOffset == n {
+					ret[n] = device
+				}
+			}
+		}
+	}
+	return ret
+}
+
+func (node *Node) RequestSegment(peer protocol.DeviceIdentifier, segment protocol.FileSegment) {
+	timestamp := helpers.TrunkI64(time.Now().UnixMilli())
+	req := p2p.Message(p2p.GimmeFileSegmentRequest(segment, uint32(timestamp)))
+	x := &req
+	b, err := x.Serialize()
+	if err != nil {
+		return
+	}
+	node.sender.Send(string(peer), b)
+}
--- node/lib/node.go
+++ node/lib/node.go
@@ -5,9 +5,6 @@ import (
 	"cc_project/protocol/p2p"
 	"cc_project/protocol"
 	"cc_project/protocol/fstp"
-	"encoding/json"
-	"fmt"
-	"time"
 )
 
 type Node struct {
@@ -39,79 +36,4 @@ func NewNode(fstp_config fstp.Config, p2p_config p2p.Config) (*Node, error) {
 	client.P2PConfig = p2p_config
 	client.sender = helpers.NewUploader(5)
 	return client, err
-}
-
-func (node *Node) DownloadFile(file_hash protocol.FileHash) error {
-	if _, ok := node.Chanels.Get(file_hash); ok {
-		return fmt.Errorf("download already in progress")
-	}
-
-	file_meta_data, ok := node.KnownFiles[file_hash] // file_meta_data,ok := c.KnownFiles.get(file_hash)
-
-	if !ok {
-		node.FetchFiles(nil)
-		file_meta_data, ok = node.KnownFiles[file_hash]
-		if !ok {
-			return fmt.Errorf("files does not exist: %v", file_hash)
-		}
-	}
-
-	resp, err := node.FSTPclient.Request(fstp.NewWhoHasRequest(fstp.WhoHasReqProps{File: file_hash}))
-	if err != nil {
-		return err
-	}
-	pay, ok := resp.Payload.(*fstp.WhoHasRespProps)
-	if !ok {
-		return fmt.Errorf("invalid payload")
-	}
-	p := map[protocol.DeviceIdentifier][]protocol.FileSegment(*pay)
-	if resp.Header.Flags == fstp.ErrResp {
-		err_resp := resp.Payload.(*fstp.ErrorResponse)
-		return fmt.Errorf(err_resp.Err)
-	}
-	go node.send_segment_requests(p)
-	go node.await_segment_responses(file_meta_data)
-
-	return nil
-}
-
-func (node *Node) send_segment_requests(m map[protocol.DeviceIdentifier][]protocol.FileSegment) {
-	for id, segments := range m {
-		for _, segment := range segments {
-			node.RequestSegment(id, segment)
-		}
-	}
-}
-
-func (node *Node) await_segment_responses(file protocol.FileMetaData) {
-	ch, _ := node.Chanels.Get(file.Hash)
-	for msg := range ch {
-		println(json.Marshal(msg))
-	}
-
-}
-
-func (node *Node) Distribute(device_segments map[protocol.DeviceIdentifier][]protocol.FileSegment, metadata protocol.FileMetaData) map[int64]protocol.DeviceIdentifier {
-	ret := make(map[int64]protocol.DeviceIdentifier)
-	for n := int64(0); n < int64(metadata.Length/protocol.SegmentLength); n++ {
-		for device, segments := range device_segments {
-			for _, segment := range segments {
-				if segment.BlockOffset == n {
-					ret[n] = device
-				}
-			}
-		}
-	}
-	return ret
-}
-
-func (node *Node) RequestSegment(peer protocol.DeviceIdentifier, segment protocol.FileSegment) {
-	timestamp := helpers.TrunkI64(time.Now().UnixMilli())
-	req := p2p.Message(p2p.GimmeFileSegmentRequest(segment, uint32(timestamp)))
-	x := &req
-	b, err := x.Serialize()
-	if err != nil {
-		return
-	}
-	node.sender.Send(string(peer), b)
 }
